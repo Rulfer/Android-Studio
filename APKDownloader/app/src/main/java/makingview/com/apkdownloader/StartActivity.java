b220f33d87df4e8d789ba99829d36dbb9b2dcba6
Bård Harald <lol_controll@hotmail.com>
2016-10-21 13:11:29 +0200

    APKDownloader: Can download files. Next I need to actually download APK files, and initiate the install process right after it's downloaded

--- APKDownloader/app/src/main/java/makingview/com/apkdownloader/StartActivity.java
+++ APKDownloader/app/src/main/java/makingview/com/apkdownloader/StartActivity.java
@@ -1,35 +1,182 @@
 package makingview.com.apkdownloader;
 
+import android.app.DownloadManager;
+import android.app.DownloadManager.Query;
+import android.app.DownloadManager.Request;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.logging.Logger;
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class StartActivity extends AppCompatActivity {
+//    Download m_Download = new Download();
+//    ProgressDialog mProgressDialog = null;
+    String downloadURL = "";
+    String apkName = "";
+
+    private long myDownloadReferende;
+    private BroadcastReceiver recieverDownloadComplete;
+    private BroadcastReceiver recieverNotificationClicked;
+
+    private long enqueue;
+    private DownloadManager dm;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_layout);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        /*mProgressDialog = new ProgressDialog(StartActivity.this);
+        mProgressDialog.setMessage("A message");
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        mProgressDialog.setCancelable(true);
+        */
+
+        BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+                    long downloadId = intent.getLongExtra(
+                            DownloadManager.EXTRA_DOWNLOAD_ID, 0);
+                    Query query = new Query();
+                    query.setFilterById(enqueue);
+                    Cursor c = dm.query(query);
+                    if (c.moveToFirst()) {
+                        int columnIndex = c
+                                .getColumnIndex(DownloadManager.COLUMN_STATUS);
+                        if (DownloadManager.STATUS_SUCCESSFUL == c
+                                .getInt(columnIndex)) {
+
+                            Log.d("....", "did you just download the file? : O ");
+                           // String savedPath =
+                            Log.d("uri", "asd");
+
+                            Integer uriIndex = c.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI);
+
+                        }
+                    }
+                }
+            }
+        };
+
+        registerReceiver(receiver, new IntentFilter(
+                DownloadManager.ACTION_DOWNLOAD_COMPLETE));
     }
 
-    public void installMovieMenu(View view)
+    public void installDownloadedAPK()
     {
         Intent promptInstall = new Intent(Intent.ACTION_VIEW)
-                .setDataAndType(Uri.parse("file:///sdcard/Download/MovieMenu.apk"),
+                .setDataAndType(Uri.parse(Environment.DIRECTORY_DOWNLOADS + apkName),
                         "application/vnd.android.package-archive");
         startActivity((promptInstall));
     }
 
     public void downloadPano(View view)
     {
-        String downloadPath;
+        downloadURL = "http://video.makingview.no/apps/gearVR/360videos/2012_Red_Bull_F1.p-experience";
+        InitDownload(downloadURL);
     }
 
     public void downloadMenu(View view)
     {
-        String downloadPath;
+        downloadURL = "http://video.makingview.no/apps/gearVR/360videos/2012_Red_Bull_F1.p-experience";
+        InitDownload(downloadURL);
     }
 
+    void InitDownload(String path)
+    {
+        dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        DownloadManager.Request request = new DownloadManager.Request(
+                Uri.parse(path));
+        String savePath = Environment.DIRECTORY_DOWNLOADS;
+        String fileName = "Red Bull F1.p-experience";
+        apkName = fileName;
+        Log.d("asd0", savePath);
+        request.setDestinationInExternalFilesDir(StartActivity.this, savePath, fileName);
+        enqueue = dm.enqueue(request);
+    }
+
+    public void showDownload(View view) {
+        Intent i = new Intent();
+        i.setAction(DownloadManager.ACTION_VIEW_DOWNLOADS);
+        startActivity(i);
+    }
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Start Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
+
+
