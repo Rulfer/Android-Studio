3a404dd2982faac6b782738f6b6f6e1faa8f5b09
Bård Harald <lol_controll@hotmail.com>
2016-11-03 14:47:11 +0100

    Added two separate buttons: One to launch Movie Menu, one to download and install update (random download file atm)

---
+++ WidgetExample/app/src/main/java/com/makingview/widgetexample/DownloadClass.java
@@ -0,0 +1,84 @@
+package com.makingview.widgetexample;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class DownloadClass
+{
+    private DownloadManager downloadManager;
+    Long queueID;
+
+    public DownloadClass(Uri uri, Context context)
+    {
+        queueID = DownloadData(uri, context);
+    }
+
+    private long DownloadData (Uri uri, Context context) {
+
+        long downloadReference;
+
+        downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        //Setting title of request
+        request.setTitle("Downloading an update for something.");
+
+        //Setting description of request
+        request.setDescription("Android Data download using DownloadManager.");
+
+        //Set the local destination for the downloaded file to a path within the application's external files directory
+        request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, "tempapk.apk"); //Saveposition of APK
+
+
+
+        //Enqueue download and save the referenceId
+        downloadReference = downloadManager.enqueue(request);
+
+        /*View cancelButton = findViewById(R.id.cancel_download);
+        cancelButton.setEnabled(true);
+        cancelButton.setVisibility(View.VISIBLE);
+
+        Button LayoutButton = (Button) findViewById(R.id.lay_below_me);
+        LayoutButton.setVisibility(View.INVISIBLE);*/
+
+        return downloadReference;
+    }
+
+    private BroadcastReceiver downloadReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            DownloadManager.Query DownloadQuery = new DownloadManager.Query();
+            DownloadQuery.setFilterById(queueID);
+
+            Cursor cursor = downloadManager.query(DownloadQuery);
+            if(cursor.moveToFirst())
+            {
+                DownloadManager.Query query = new DownloadManager.Query();
+                query.setFilterById(intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1));
+                cursor.moveToFirst();
+                int status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                //This is String I pass to openFile method
+                String savedFilePath = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_FILENAME));
+                //installDownloadedAPK(savedFilePath);
+            }
+            else
+            {
+                Toast toast = Toast.makeText(context,
+                        "Download Cancelled", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.TOP, 25, 400);
+                toast.show();
+            }
+        }
+    };
+}
--- WidgetExample/app/src/main/java/com/makingview/widgetexample/ExampleAppWidgetProvider.java
+++ WidgetExample/app/src/main/java/com/makingview/widgetexample/ExampleAppWidgetProvider.java
@@ -5,22 +5,34 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 
+import android.app.DownloadManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 public class ExampleAppWidgetProvider extends AppWidgetProvider {
     DateFormat df = new SimpleDateFormat("hh:mm:ss");
     AppCompatActivity app = new AppCompatActivity();
+    DownloadClass dc;
+    Long queueID;
+    private DownloadManager downloadManager;
 
-    private static final String MyOnClick = "myOnClickTag";
+    private static final String MyOnClick = "launcher";
+    private static final String MyOnClick1 = "updater";
 
 
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -32,24 +44,15 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
         // provider
         for (int i = 0; i < N; i++) {
             int appWidgetId = appWidgetIds[i];
-            Log.d("fuck me", "3");
-
-
-            // Create an Intent to launch ExampleActivity
-            //Intent intent = new Intent(context, ExampleAppWidgetProvider.class);
-
-            //Intent intent = app.getPackageManager().getLaunchIntentForPackage("com.makingview.MovieMenu");
-
-            //PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
 
             // Get the layout for the App Widget and attach an on-click listener
             // to the button
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget1);
             views.setOnClickPendingIntent(R.id.button, getPendingSelfIntent(context, MyOnClick));
+            views.setOnClickPendingIntent(R.id.update, getPendingSelfIntent1(context, MyOnClick1));
 
             // To update a label
-            views.setTextViewText(R.id.widget1label, df.format(new Date()));
+            //views.setTextViewText(R.id.widget1label, df.format(new Date()));
 
             // Tell the AppWidgetManager to perform an update on the current app
             // widget
@@ -63,33 +66,44 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
         return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
 
+    protected PendingIntent getPendingSelfIntent1(Context context, String action) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
+
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);//add this line
-        Log.d("fuck me", "|");
 
         if (MyOnClick.equals(intent.getAction()))
         {
-            Log.d("fuck me", "2");
-            //your onClick action is here
             openApp(context);
+        }
+        else if(MyOnClick1.equals(intent.getAction()))
+        {
+            updateApp(context);
         }
     };
 
-    public static void openApp(Context context)
+    public void openApp(Context context)
     {
         PackageManager manager = context.getPackageManager();
-        Log.d("fuck me", "3");
 
-        //Intent i = manager.getLaunchIntentForPackage("com.MakingView.movieMenu");
-        Intent i = manager.getLaunchIntentForPackage("com.MakingView.singleMovieplayer");
-        Log.d("fuck me", "4");
+        Intent i = manager.getLaunchIntentForPackage("com.MakingView.movieMenu");
 
         if(i != null)
         {
-            Log.d("fuck me", "5");
-
             i.addCategory(Intent.CATEGORY_LAUNCHER);
             context.startActivity(i);
         }
     }
+
+    public void updateApp(Context context)
+    {
+        Uri urlUri = Uri.parse("http://video.makingview.no/apps/gearVR/360videos/2016_Audi_Golf_Sport.m-experience");
+
+        dc = new DownloadClass(urlUri, context);
+    }
+
+
 }
