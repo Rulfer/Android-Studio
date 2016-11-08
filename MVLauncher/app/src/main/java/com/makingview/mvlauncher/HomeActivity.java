e75a677f9246a8264cb36461292c4fe1a2043cd8
Bård Harald <lol_controll@hotmail.com>
2016-11-08 10:06:22 +0100

    This works, test some changes

--- MVLauncher/app/src/main/java/com/makingview/mvlauncher/HomeActivity.java
+++ MVLauncher/app/src/main/java/com/makingview/mvlauncher/HomeActivity.java
@@ -2,6 +2,7 @@ package com.makingview.mvlauncher;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -17,8 +18,18 @@ public class HomeActivity extends Activity
         setContentView(R.layout.activity_home);
     }
 
-    public void showApps(View view){
-        Intent i = new Intent(this, AppListActivity.class);
-        startActivity(i);
+    public void openMovieMenu(View view){
+        //Intent i = new Intent(this, AppListActivity.class);
+        //startActivity(i);
+
+        PackageManager manager = getPackageManager();
+
+        Intent i = manager.getLaunchIntentForPackage("com.MakingView.movieMenu");
+
+        if(i != null)
+        {
+            i.addCategory(Intent.CATEGORY_LAUNCHER);
+            startActivity(i);
+        }
     }
 }
